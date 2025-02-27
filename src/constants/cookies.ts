9549e0489a916a1d5d648190c6d103c3e07ffa57
Jesse Koldewijn <jesse.koldewijn@justeattakeaway.com>
2025-02-27 23:33:11 +0100

    feat: added prep for custom bangs through cookies

--- src/constants/cookies.ts
+++ src/constants/cookies.ts
@@ -1,5 +1,6 @@
 export const COOKIES = {
 	DEFAULT_BANG: "default-bang",
+	CUSTOM_BANGS: "custom-bangs",
 } as const;
 export type CookieName = (typeof COOKIES)[keyof typeof COOKIES];
 export type CookieNames = CookieName[];
